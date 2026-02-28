8f071286dfa6000b82dc3b8e49a96366f2774f50
bkandh30 <kandhari.bhavya20@gmail.com>
2026-02-28 12:10:00 -0700

    Added clerk support scaffolding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,7 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased`}
       >
@@ -32,5 +35,7 @@ export default function RootLayout({
         <Toaster />
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
